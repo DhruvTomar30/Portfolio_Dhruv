eeef7c8c3f225a10f950ebbc52377d3161484cbc
Dhruv Tomar <dhruvtomar2020@gmail.com>
2025-04-09 16:50:35 +0530

    ....

---
+++ gulpfile.mjs
@@ -0,0 +1,121 @@
+// Node.js Packages / Dependencies
+import gulp from 'gulp';
+import gulpSass from 'gulp-sass';
+import uglify from 'gulp-uglify';
+import rename from 'gulp-rename';
+import concat from 'gulp-concat';
+import cleanCSS from 'gulp-clean-css';
+import imagemin from 'gulp-imagemin';
+import pngQuant from 'imagemin-pngquant';
+import browserSyncLib from 'browser-sync';
+import autoprefixer from 'gulp-autoprefixer';
+import jpgRecompress from 'imagemin-jpeg-recompress';
+import clean from 'gulp-clean';
+import sassCompiler from 'sass';
+
+// Initialize BrowserSync and Sass
+const browserSync = browserSyncLib.create();
+const sass = gulpSass(sassCompiler);
+
+// Paths
+const paths = {
+    root: { www: './public_html' },
+    src: {
+        root: 'public_html/assets',
+        html: 'public_html/**/*.html',
+        css: 'public_html/assets/css/*.css',
+        js: 'public_html/assets/js/*.js',
+        vendors: 'public_html/assets/vendors/**/*.*',
+        imgs: 'public_html/assets/imgs/**/*.+(png|jpg|gif|svg)',
+        scss: 'public_html/assets/scss/**/*.scss'
+    },
+    dist: {
+        root: 'public_html/dist',
+        css: 'public_html/dist/css',
+        js: 'public_html/dist/js',
+        imgs: 'public_html/dist/imgs',
+        vendors: 'public_html/dist/vendors'
+    }
+};
+
+// Compile SCSS
+export function sassTask() {
+    return gulp.src(paths.src.scss)
+        .pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))
+        .pipe(autoprefixer())
+        .pipe(gulp.dest(paths.src.root + '/css'))
+        .pipe(browserSync.stream());
+}
+
+// Minify + Combine CSS
+export function cssTask() {
+    return gulp.src(paths.src.css)
+        .pipe(cleanCSS({ compatibility: 'ie8' }))
+        .pipe(concat('johndoe.css'))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest(paths.dist.css));
+}
+
+// Minify + Combine JS
+export function jsTask() {
+    return gulp.src(paths.src.js)
+        .pipe(uglify())
+        .pipe(concat('johndoe.js'))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest(paths.dist.js));
+}
+
+// Optimize Images
+export function imagesTask() {
+    return gulp.src(paths.src.imgs)
+        .pipe(imagemin([
+            imagemin.gifsicle({ interlaced: true }),
+            imagemin.mozjpeg({ quality: 75, progressive: true }),
+            imagemin.optipng({ optimizationLevel: 5 }),
+            pngQuant(),
+            jpgRecompress()
+        ]))
+        .pipe(gulp.dest(paths.dist.imgs));
+}
+
+// Copy Vendors
+export function vendorsTask() {
+    return gulp.src(paths.src.vendors)
+        .pipe(gulp.dest(paths.dist.vendors));
+}
+
+// Clean Dist
+export function cleanDistTask() {
+    return gulp.src(paths.dist.root, { allowEmpty: true, read: false })
+        .pipe(clean());
+}
+
+// BrowserSync + Watch
+export function serveTask() {
+    browserSync.init({
+        server: {
+            baseDir: paths.root.www
+        },
+        port: 3000
+    });
+
+    gulp.watch(paths.src.scss, sassTask);
+    gulp.watch(paths.src.css, cssTask).on('change', browserSync.reload);
+    gulp.watch(paths.src.js, jsTask).on('change', browserSync.reload);
+    gulp.watch(paths.src.html).on('change', browserSync.reload);
+}
+
+// Build Task
+export const build = gulp.series(
+    cleanDistTask,
+    gulp.parallel(sassTask, cssTask, jsTask, imagesTask, vendorsTask)
+);
+
+// Dev Task
+export const dev = gulp.series(
+    build,
+    serveTask
+);
+
+// Default Task
+export default dev;
